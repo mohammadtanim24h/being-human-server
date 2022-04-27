87d88986b1dcb4338a87070c804e92fb98f01788
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-27 10:05:06 +0600

    initialized server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// beingAdmin
+// 0JGFVBTT9B9SVZZn
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+//Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efjqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const activityCollection = client.db("beingHuman").collection("volunteerActivities");
+        
+        // Get all activities 
+        app.get("/activities", async (req, res) => {
+            const query = req.query;
+            const cursor = activityCollection.find(query);
+            const activities = await cursor.toArray();
+            res.send(activities);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Being Human Server is up and running!");
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
